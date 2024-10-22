51a379180751002a89f27b202157ac67dfca0be9
Aries Powvalla <aries@ninjam.co>
2024-10-22 10:32:00 -0700

    Update manage.js

--- public/manage/manage.js
+++ public/manage/manage.js
@@ -478,71 +478,71 @@ socket.emit("findUsers", "#", (response) => {
 
         //
 
-        uData.forEach((s) => {
-            var recd = "";
+//         uData.forEach((s) => {
+//             var recd = "";
 
-            console.log(s.id);
+//             console.log(s.id);
 
-            if (s.record === undefined || s.record.length == 0) {
-                const textField = document.createElement("p");
-                recd = "-";
-            } else {
-                const reversedData = [...s.record].reverse();
-                // console.log(reversedData);
+//             if (s.record === undefined || s.record.length == 0) {
+//                 const textField = document.createElement("p");
+//                 recd = "-";
+//             } else {
+//                 const reversedData = [...s.record].reverse();
+//                 // console.log(reversedData);
 
-                let rtindex = -1;
+//                 let rtindex = -1;
 
-                while (recd == "") {
-                    rtindex += 1;
-                    ritem = reversedData[rtindex];
-                    console.log(ritem);
-                    const timeDifference = new Date(ritem.end) - new Date(ritem.start);
-                    console.log(timeDifference);
-                    if (!(timeDifference > 0 && timeDifference <= 10 * 60 * 1000)) {
-                        recd = formatDate1(ritem.start, ritem.end);
-                        console.log(recd);
-                    }
-                    // If we reach the end of the array and still haven't found a valid entry, set recd to "N/A"
-                    if (rtindex === reversedData.length - 1 && recd == "") {
-                        recd = "-";
-                    }
-                }
-            }
+//                 while (recd == "") {
+//                     rtindex += 1;
+//                     ritem = reversedData[rtindex];
+//                     console.log(ritem);
+//                     const timeDifference = new Date(ritem.end) - new Date(ritem.start);
+//                     console.log(timeDifference);
+//                     if (!(timeDifference > 0 && timeDifference <= 10 * 60 * 1000)) {
+//                         recd = formatDate1(ritem.start, ritem.end);
+//                         console.log(recd);
+//                     }
+//                     // If we reach the end of the array and still haven't found a valid entry, set recd to "N/A"
+//                     if (rtindex === reversedData.length - 1 && recd == "") {
+//                         recd = "-";
+//                     }
+//                 }
+//             }
 
-            const validMeetings =
-                s?.record?.filter(
-                    (e) => new Date(e.end) - new Date(e.start) > 15 * 60 * 1000
-                ) || [];
+//             const validMeetings =
+//                 s?.record?.filter(
+//                     (e) => new Date(e.end) - new Date(e.start) > 15 * 60 * 1000
+//                 ) || [];
 
-            const avgm =
-                validMeetings.length > 0
-                    ? (
-                        validMeetings.reduce(
-                            (t, e) => t + (new Date(e.end) - new Date(e.start)),
-                            0
-                        ) /
-                        (validMeetings.length * 3600000)
-                    ).toFixed(3)
-                    : "-";
+//             const avgm =
+//                 validMeetings.length > 0
+//                     ? (
+//                         validMeetings.reduce(
+//                             (t, e) => t + (new Date(e.end) - new Date(e.start)),
+//                             0
+//                         ) /
+//                         (validMeetings.length * 3600000)
+//                     ).toFixed(3)
+//                     : "-";
 
-            if (s.hours == 0) {
-                hrd = "-";
-            } else {
-                hrd = s.hours;
-            }
+//             if (s.hours == 0) {
+//                 hrd = "-";
+//             } else {
+//                 hrd = s.hours;
+//             }
 
-            document.getElementById("list").innerHTML += `<tr>
-    <td><span class="text-muted">${numy}</span></td>
-    <td><a class="text-reset" tabindex="-1">${s.name}</a></td>
-    <td>${s.id}</td>
-    <td>${hrd}</td>
-    <td>${avgm}</td>
-    <td>20${s.grad}</td>
-    <td>${recd}</td>
-  </tr>`;
+//             document.getElementById("list").innerHTML += `<tr>
+//     <td><span class="text-muted">${numy}</span></td>
+//     <td><a class="text-reset" tabindex="-1">${s.name}</a></td>
+//     <td>${s.id}</td>
+//     <td>${hrd}</td>
+//     <td>${avgm}</td>
+//     <td>20${s.grad}</td>
+//     <td>${recd}</td>
+//   </tr>`;
 
-            numy += 1;
-        });
+//             numy += 1;
+//         });
     } else {
         alert("There was an error!");
     }
